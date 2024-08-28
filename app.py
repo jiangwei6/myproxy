ab88f25005ab99a7130c4f9f283fe9725a020c4c
jiangwei6 <jiangwei6@huawei.com>
2024-08-28 20:16:31 +0800

    Update app.py
    3

--- app.py
+++ app.py
@@ -3,17 +3,28 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/proxy', methods=['POST'])
+@app.route('/proxy', methods=['GET', 'POST'])
 def proxy():
     try:
-        data = request.json
-        url = data.get('url')
-        method = data.get('method', 'GET')
-        headers = data.get('headers', {})
-        payload = data.get('data', {})
+        if request.method == 'POST':
+            data = request.json
+            url = data.get('url')
+            method = data.get('method', 'GET')
+            headers = data.get('headers', {})
+            payload = data.get('data', {})
 
-        # 发起 HTTP 请求到目标 API
-        response = requests.request(method, url, headers=headers, json=payload)
+            # 发起 HTTP 请求到目标 API
+            response = requests.request(method, url, headers=headers, json=payload)
+        
+        elif request.method == 'GET':
+            # 从查询参数中获取数据
+            url = request.args.get('url')
+            method = request.args.get('method', 'GET')
+            headers = request.args.get('headers', {})
+            payload = request.args.get('data', {})
+
+            # 发起 HTTP 请求到目标 API
+            response = requests.request(method, url, headers=headers, json=payload)
 
         # 返回目标 API 的响应给客户端
         return jsonify(response.json()), response.status_code
